ae96d5fcefda618a35b523defa70a9f4adac0d04
KappaKippo <kostasrdg@gmail.com>
2024-01-31 22:02:06 +0200

    New: Progress functionality of API Add: .env .env.example Add: More libraries

--- hapi/forms.py
+++ hapi/forms.py
@@ -3,3 +3,14 @@ from django import forms
 
 class SearchForm(forms.Form):
     user_search = forms.CharField(label="Your Search", max_length=100)
+    orientation = forms.CharField(
+        label="Orientation",
+        max_length=20,
+        required=False,
+        help_text="Valid values are: landscape, portrait, squarish"
+    )
+    color = forms.CharField(
+        label="Color",
+        max_length=20,
+        required=False,
+    )
--- hapi/views.py
+++ hapi/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+import requests
+import random
+from unsplash.settings import CLIENT_ID
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
 
 from .forms import SearchForm
 
@@ -9,11 +12,30 @@ def search(request):
     if request.method == "GET":
         # Create a form instance and populate it with request data
         form = SearchForm(request.GET)
+
         # check the validation of form
         if form.is_valid():
             # We have to validate the data here
-            # Redirect to a new URL
-            return HttpResponse("Site Under Construction")
+            search_term = form.cleaned_data["user_search"]
+            orientation = form.cleaned_data['orientation']
+            color = form.cleaned_data['color']
+            payload = {
+                'client_id': CLIENT_ID,
+                'query': search_term,
+                'orientation': orientation if orientation else None,
+                'color': color if color else None,
+            }
+
+            # Make the response to Unsplash
+            response = requests.get('https://api.unsplash.com/search/photos/', stream=True, params=payload)
+
+            # Get response data and select one random image to redirect to
+            results = response.json().get('results')
+            choice = random.choice(range(0, len(results)))
+            image_url = results[choice].get('urls').get('raw')
+
+            return HttpResponseRedirect(image_url)
+
     # If we receive any other form that POST we will return a blank form
     else:
         form = SearchForm()
